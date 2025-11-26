3a43ee3c1e6b4ab258d4f338f3a9627e4785103c
NoumanMalik <NoumanMalik3000@outlook.com>
2025-11-27 02:25:01 +0500

    untrack file

--- src/CleanArch.Application/Interfaces/ISalesStatsRepository.cs
+++
@@ -1,8 +0,0 @@
-using CleanArch.Domain.Entities;
-
-namespace CleanArch.Application.Interfaces;
-
-public interface ISalesStatsRepository
-{
-    Task<IEnumerable<SalesStat>> GetSalesStatsAsync();
-}
