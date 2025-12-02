3bd530cc93803612464955e8075be4b306cec5eb
Nouman Waheed <noumanmalik3000@outlook.com>
2025-12-02 19:10:34 +0500

    code added for account gorup and account name

--- src/AdvAsmPlanning.Infrastructure/Services/AccountService.cs
+++ src/AdvAsmPlanning.Infrastructure/Services/AccountService.cs
@@ -17,14 +17,14 @@ public class AccountService(IRepository<Account> repository) : IAccountService
 
         IEnumerable<DropdownDto> dropdowns = key switch
         {
-            "c_AccountSubgroup" => accounts
-                .Where(d => !string.IsNullOrEmpty(d.c_AccountSubgroup))
-                .DistinctBy(d => d.c_AccountSubgroup)
+            "AccountName" => accounts
+                .Where(d => !string.IsNullOrEmpty(d.Code) || !string.IsNullOrEmpty(d.Description))
                 .Select(d => new DropdownDto
                 {
-                    Label = d.c_AccountSubgroup,
-                    Value = d.c_AccountSubgroup
-                }),
+                    Label = $"{d.Code}_{d.Description}",
+                    Value = $"{d.Code}_{d.Description}"
+                })
+                .Distinct(),
             "c_ExtReport" => accounts
                 .Where(d => !string.IsNullOrEmpty(d.c_ExtReport))
                 .DistinctBy(d => d.c_ExtReport)
@@ -33,7 +33,26 @@ public class AccountService(IRepository<Account> repository) : IAccountService
                     Label = d.c_ExtReport,
                     Value = d.c_ExtReport
                 }),
-            _ => Enumerable.Empty<DropdownDto>()
+            "c_Accountgroup" => accounts
+                .Join(accounts,
+                      a => a.MemberId,
+                      acc => acc.MemberId,
+                      (a, acc) => acc)
+                .Where(acc => !string.IsNullOrEmpty(acc.c_RollupLevel1))
+                .Select(d => new DropdownDto
+                {
+                    Label = d.c_RollupLevel1.Length < 4 ? d.c_RollupLevel1.Substring(d.c_RollupLevel1.Length - 3) : d.c_RollupLevel1.Substring(3),
+                    Value = d.c_RollupLevel1.Length < 4 ? d.c_RollupLevel1.Substring(d.c_RollupLevel1.Length - 3) : d.c_RollupLevel1.Substring(3)
+                }).Distinct(),
+            "c_AccountSubgroup" => accounts
+                .Where(d => !string.IsNullOrEmpty(d.c_AccountSubgroup))
+                .DistinctBy(d => d.c_AccountSubgroup)
+                .Select(d => new DropdownDto
+                {
+                    Label = d.c_AccountSubgroup,
+                    Value = d.c_AccountSubgroup
+                }),
+            _ => []
         };
 
         return ApiResponseDto<IEnumerable<DropdownDto>>.SuccessResponse(dropdowns, dropdowns.LongCount());
